1bc896b1ed98ee51938ee6b796cc39110a3f0a7b
muralim007-hub <murali.m007@gmail.com>
2026-01-07 15:11:17 +0530

    Add files via upload

---
+++ assignment1.2_basic_python_concept.py
@@ -0,0 +1,5 @@
+"""Task 2: Create a Personalized Greeting"""
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+name1=first_name+" "+last_name
+print("Hello",name1,"welcome to python program")
---
+++ assignment1_basic_python_concept.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter a number: "))
+num2=int(input("Enter another number: "))
+addition=num1+num2
+subtraction=(num1-num2)
+multiplication=num1*num2
+division=num1/num2
+print("addition:",addition)
+print("subtraction:",subtraction)
+print("multiplication:",multiplication)
+print("division:",division)
+
+
