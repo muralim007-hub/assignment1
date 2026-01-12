9319bcd9bfa2a259b120813bc562a9cdc97bf907
muralim007-hub <murali.m007@gmail.com>
2026-01-12 17:21:12 +0530

    Update assignment1.2_basic_python_concept.py

--- assignment1.2_basic_python_concept.py
+++ assignment1.2_basic_python_concept.py
@@ -1,5 +1,17 @@
 """Task 2: Create a Personalized Greeting"""
-first_name=input("Enter your first name: ")
-last_name=input("Enter your last name: ")
-name1=first_name+" "+last_name
-print("Hello",name1,"welcome to python program")
+
+# 1. Ask the user for their first name
+first_name = input("Enter your first name: ")
+
+# 2. Ask the user for their last name
+last_name = input("Enter your last name: ")
+
+# 3. Combine first and last name into one string
+name1 = first_name + " " + last_name
+
+# 4. Print a personalized greeting
+print("Hello", name1, "welcome to python program")
+
+
+
+
