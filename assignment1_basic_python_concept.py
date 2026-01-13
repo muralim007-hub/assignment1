05e38bf7c0daee97482a6a5e9897fc34d662e6ef
muralim007-hub <murali.m007@gmail.com>
2026-01-13 23:12:11 +0530

    Update assignment1_basic_python_concept.py

--- assignment1_basic_python_concept.py
+++ assignment1_basic_python_concept.py
@@ -1,3 +1,9 @@
+"""Task1: Write a Python program that does the following:
+Takes two numbers as input from the user.
+Performs the basic mathematical operations on these two numbers: Addition Subtraction Multiplication Division
+Displays the results of each operation on the screen."""
+
+
 num1=int(input("Enter a number: "))
 num2=int(input("Enter another number: "))
 addition=num1+num2
@@ -8,5 +14,6 @@ print("addition:",addition)
 print("subtraction:",subtraction)
 print("multiplication:",multiplication)
 print("division:",division)
+
 
 
